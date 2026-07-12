4e737d913f9a0c9ca66b4ec1f069cf2c97f60d97
Tanika Gupta <insightsbytanika@gmail.com>
2026-07-12 23:25:10 +0530

    all imports working

--- dashboard/app.py
+++ dashboard/app.py
@@ -11,3 +11,12 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from config import DIR_PROCESSED, DIR_PRICES
+
+#Page config
+st.set_page_config(
+    page_title="AlphaLens",
+    page_icon="📈",
+    layout="wide",
+)
+
+print("imports working")
